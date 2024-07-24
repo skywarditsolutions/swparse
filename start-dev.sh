8ae57400ff6e859a5276dfc8a4b58d45306cbd34
Phyo Arkar Lwin <phyoakl@hexcode.tech>
2024-07-25 05:31:33 +0700

    fix: docker command

--- start-dev.sh
+++ start-dev.sh
@@ -3,8 +3,8 @@ cp .env.example .env
 source .env
 if [ ! -d "./data/" ]; then mkdir -p ./data/; fi
 if [ ! -d "./data/$MINIO_BUCKET" ]; then mkdir -p ./data/$MINIO_BUCKET; fi
-docker-compose pull
-docker-compose up --remove-orphans
+docker compose pull
+docker compose up --build --remove-orphans
 mc='docker exec -it minio-client mc'
 # $mc admin user add minio/ $MINIO_CONSOLE_USER $MINIO_CONSOLE_PASSWORD
 # $mc admin policy add minio/ consoleAdmin /root/.mc/admin.json
