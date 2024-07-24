dab44f61aed18bee694ae3884ddbd4cac80cddca
Phyo Arkar Lwin <phyoakl@hexcode.tech>
2024-07-25 05:40:56 +0700

    feat: docker compose file for prod , runs on port 80

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 source .env
 if [ ! -d "./data/" ]; then mkdir -p ./data/; fi
 if [ ! -d "./data/$MINIO_BUCKET" ]; then mkdir -p ./data/$MINIO_BUCKET; fi
-docker compose pull
-docker compose up --build --remove-orphans -d
+docker compose pull -f docker-compose.prod.yaml
+docker compose up -f docker-compose.prod.yaml --build --remove-orphans -d
 mc='docker exec -it minio-client mc'
 # $mc admin user add minio/ $MINIO_CONSOLE_USER $MINIO_CONSOLE_PASSWORD
 # $mc admin policy add minio/ consoleAdmin /root/.mc/admin.json
