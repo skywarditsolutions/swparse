106d9568fab171b14c3c9d086c9d161151bbdaff
Phyo Arkar Lwin <phyoakl@hexcode.tech>
2024-07-30 04:36:22 +0700

    fix: makle sure env is sourced

--- start-prod.sh
+++ start-prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export MODE="prod"
 cp .env.example .env
+source .env
 if [ ! -d "./data/" ]; then mkdir -p ./data/; fi
 if [ ! -d "./data/$MINIO_BUCKET" ]; then mkdir -p ./data/$MINIO_BUCKET; fi
 docker compose -f docker-compose.prod.yaml up --build --remove-orphans -d
