9d7aaf640d69118c8c39b763e3ca1800337b12ad
khinyu <khinyuwaddythin@hexcode.tech>
2024-10-12 18:44:51 +0630

    fix: add configs for local env

--- src/swparse/config/base.py
+++ src/swparse/config/base.py
@@ -398,7 +398,7 @@ class MinioSettings:
     ROOT_USER: str = field(default_factory=lambda: os.getenv("MINIO_ROOT_USER", "admin"))
     ROOT_PASSWORD: str = field(default_factory=lambda: os.getenv("MINIO_ROOT_PASSWORD", "password"))
     BUCKET: str = field(default_factory=lambda: os.getenv("MINIO_BUCKET", "swparse"))
-    ENDPOINT_URL: str = field(default_factory=lambda: os.getenv("MINIO_ENDPOINT", "http://minio:9000/"))
+    ENDPOINT_URL: str = field(default_factory=lambda: os.getenv("MINIO_ENDPOINT_URL", "http://minio:9000/"))
 
 
 @dataclass
