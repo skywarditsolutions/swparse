0543bcafc12eb92551aa7cc24661a14541466662
Kaung Zin Hein <kgzinhein.my@gmail.com>
2024-10-29 16:49:25 +0700

    fix: disable pptx conversion, remove pptx2md

--- src/swparse/domain/swparse/utils.py
+++ src/swparse/domain/swparse/utils.py
@@ -5,8 +5,9 @@ from uuid import uuid4
 from lxml import html
 from s3fs import S3FileSystem
 from xls2xlsx import XLS2XLSX
-from pptx2md import outputter, parser
-from pptx import Presentation
+
+# from pptx2md import outputter, parser
+# from pptx import Presentation
 from swparse.config.app import settings
 import re
 
@@ -15,12 +16,13 @@ MINIO_ROOT_USER = settings.storage.ROOT_USER
 MINIO_ROOT_PASSWORD = settings.storage.ROOT_PASSWORD
 
 s3 = S3FileSystem(
-            # asynchronous=True,
-            endpoint_url=settings.storage.ENDPOINT_URL,
-            key=MINIO_ROOT_USER,
-            secret=MINIO_ROOT_PASSWORD,
-            use_ssl=False,
-        )
+    # asynchronous=True,
+    endpoint_url=settings.storage.ENDPOINT_URL,
+    key=MINIO_ROOT_USER,
+    secret=MINIO_ROOT_PASSWORD,
+    use_ssl=False,
+)
+
 
 def convert_xls_to_xlsx_bytes(content: bytes) -> bytes:
 
@@ -32,7 +34,8 @@ def convert_xls_to_xlsx_bytes(content: bytes) -> bytes:
         buffer.seek(0)
         return buffer.read()
 
-def save_file_s3(s3fs: S3FileSystem, file_name:str, content:bytes|str ) -> str:
+
+def save_file_s3(s3fs: S3FileSystem, file_name: str, content: bytes | str) -> str:
     new_uuid = uuid4()
     s3_url = f"{BUCKET}/{new_uuid}_{file_name}"
     if isinstance(content, str):
@@ -41,12 +44,14 @@ def save_file_s3(s3fs: S3FileSystem, file_name:str, content:bytes|str ) -> str:
         f.write(content)
         return s3_url
 
-def get_file_name(s3_url:str) ->str:
-    return os.path.basename(s3_url).split('/')[-1]
 
-def change_file_ext(file_name:str, extension:str) ->str:
+def get_file_name(s3_url: str) -> str:
+    return os.path.basename(s3_url).split("/")[-1]
+
+
+def change_file_ext(file_name: str, extension: str) -> str:
     file_path = file_name.split(".")
-    file_path[-1]=extension
+    file_path[-1] = extension
     return ".".join(file_path)
 
 
@@ -55,14 +60,14 @@ def extract_tables_from_html(s3fs: S3FileSystem, html_file_path: str) -> list[st
         content = doc.read().decode()
 
     tree = html.fromstring(content)
-    tables = tree.xpath('//table')
+    tables = tree.xpath("//table")
     if not tables:
         # No table found in the html file
         return None
 
-    html_tables:list[str] = []
+    html_tables: list[str] = []
     for table in tables:
-        html_table = html.tostring(table, method="html", pretty_print = True)
+        html_table = html.tostring(table, method="html", pretty_print=True)
 
         if isinstance(html_table, bytes):
             html_table = html_table.decode()
@@ -71,20 +76,19 @@ def extract_tables_from_html(s3fs: S3FileSystem, html_file_path: str) -> list[st
     return html_tables
 
 
-
-class CustomizedMdOutputter(outputter.md_outputter):
-    def __init__(self, s3_url: str):
-        self.ofile = s3.open(s3_url, "w")
-        self.esc_re1 = re.compile(r'([\\\*`!_\{\}\[\]\(\)#\+-\.])')
-        self.esc_re2 = re.compile(r'(<[^>]+>)')
-
+# class CustomizedMdOutputter(outputter.md_outputter):
+#     def __init__(self, s3_url: str):
+#         self.ofile = s3.open(s3_url, "w")
+#         self.esc_re1 = re.compile(r'([\\\*`!_\{\}\[\]\(\)#\+-\.])')
+#         self.esc_re2 = re.compile(r'(<[^>]+>)')
 
 
 def convert_pptx_to_md(input_url: str, output_url: str):
-    try:
-        with s3.open(input_url, "rb") as f:
-            pptx_content = Presentation(f)
-        output_obj = CustomizedMdOutputter(output_url)
-        parser.parse(pptx_content, output_obj)
-    except Exception as e:
-        raise Exception(f"Error converting PPTX to Markdown: {e}")
+    ...
+    # try:
+    #     with s3.open(input_url, "rb") as f:
+    #         pptx_content = Presentation(f)
+    #     output_obj = CustomizedMdOutputter(output_url)
+    #     parser.parse(pptx_content, output_obj)
+    # except Exception as e:
+    #     raise Exception(f"Error converting PPTX to Markdown: {e}")
